987889d083e6254403afa8d69855859fe6c6302f
Abhishek Upperwal <a.upperwal@gmail.com>
2020-07-15 23:29:19 +0530

    Updates

---
+++ precache-manifest.dcbdd287a85ad5886cbd3f93ebf0b5d6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ca160bd1ea3fba6b832c13027c0808",
+    "url": "/index.html"
+  },
+  {
+    "revision": "952fff6cfa5d4c5b8e2d",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "cd96173212f7fa1ef6b4",
+    "url": "/static/css/main.5b1c3e8e.chunk.css"
+  },
+  {
+    "revision": "952fff6cfa5d4c5b8e2d",
+    "url": "/static/js/2.6621d3e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6621d3e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd96173212f7fa1ef6b4",
+    "url": "/static/js/main.4c282a91.chunk.js"
+  },
+  {
+    "revision": "df39e605aac0331613dc",
+    "url": "/static/js/runtime-main.2f9624e8.js"
+  },
+  {
+    "revision": "9a28c02580fe3df9930e08b10dba803f",
+    "url": "/static/media/BwNistaInt-xBd.9a28c025.woff"
+  },
+  {
+    "revision": "37e3beb34cd30b55ebc6d734754d09c9",
+    "url": "/static/media/abhi.37e3beb3.png"
+  },
+  {
+    "revision": "de9fb300de0771c00a5ca9fe2fde9814",
+    "url": "/static/media/aparna.de9fb300.png"
+  },
+  {
+    "revision": "f59f4d375caebf866c0d82fac9b967a8",
+    "url": "/static/media/leaf-bg.f59f4d37.png"
+  },
+  {
+    "revision": "e7e507347ad5667efe0f6ca057ffb437",
+    "url": "/static/media/logo-03.e7e50734.svg"
+  },
+  {
+    "revision": "0484b684d7211051db887de76903a175",
+    "url": "/static/media/manuel.0484b684.png"
+  }
+]);
